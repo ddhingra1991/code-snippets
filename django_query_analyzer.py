12f1df47f60a2a406d6edce61a38c81c032ab438
Dollar Dhingra <dollar.dhingra@allyo.com>
2020-03-22 20:41:19 +0530

    added description & usage in the docstring

--- django_query_analyzer.py
+++ django_query_analyzer.py
@@ -6,7 +6,27 @@ from django.db import connection, reset_queries
 
 
 def django_query_analyze(func):
-    """decorator to perform analysis on Django queries"""
+    """decorator to perform analysis on Django queries
+    Usage:
+    place this decorator on the top of any method where you are hitting
+    some database query through ORM. This returns:
+    - no. of queries
+    - total time taken in ms
+    Eg:
+    @django_query_analyze
+    def some_method():
+        ..
+        ..
+        some_query = SomeModel.objects.all()
+        ..
+        return result
+
+    output:
+    ran function some_method
+    --------------------
+    number of queries: 1
+    Time of execution: 0.01481s
+    """
 
     def wrapper(*args, **kwargs):
 
